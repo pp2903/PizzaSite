f148bf6d61041ef0806a7d660a806ecbb7cc6989
pp2903 <priyampranshu@gmail.com>
2023-12-30 20:57:19 +0530

    Fixed add to cart

--- baseApp/static/baseApp/scripts.js
+++ baseApp/static/baseApp/scripts.js
@@ -3,6 +3,23 @@ $(document).ready(() => {
   // $('body').css('display', 'none');
   // $('body').fadeIn(2000);
 
+  //GLOBAL VARIABLES
+  var cart_storage = JSON.parse(localStorage.getItem("cart"));
+  var cart_items_json = document.getElementById("cart_items_json");
+  var cart = []; //global variable that stores the cart value that is sent to the server
+
+  for (i in cart_storage) {
+    if (cart_storage[i] === 1) {
+      cart.push(i);
+    }
+  }
+
+  for (i in cart) {
+  }
+
+  //FUNCTIONS TO LOAD ON FIRST RENDER
+  cart_items_json.value = JSON.stringify(cart);
+
   //Main Logo Animation
   $("#MainLogo").mouseover(() => {
     $("#MainLogo").css("color", "#ef7710");
@@ -60,7 +77,6 @@ $(document).ready(() => {
   );
   //social logo animations
 
-  var cart = localStorage.getItem("cart");
   if (cart === null) {
     localStorage.setItem("cart", JSON.stringify({}));
   }
@@ -68,11 +84,11 @@ $(document).ready(() => {
   var addToCart = (id) => {
     if (localStorage.getItem("cart")) {
       var cart = JSON.parse(localStorage.getItem("cart"));
-      cart[id] = true;
+      cart[id] = 1;
       localStorage.setItem("cart", JSON.stringify(cart));
     } else {
       var cart = {};
-      cart[id] = true;
+      cart[id] = 1;
       console.log(cart);
       localStorage.setItem("cart", JSON.stringify(cart));
     }
@@ -82,80 +98,113 @@ $(document).ready(() => {
   var removeProd = (id) => {
     var cart = JSON.parse(localStorage.getItem("cart"));
 
-    cart[id] = false;
+    cart[id] = 0;
     console.log(cart);
     localStorage.setItem(JSON.stringify(cart));
   };
 
   //functions that change the state of the buttons based on items in local storage
-  var cart = [] //global variable that stores the cart value that is sent to the server
-
-  var updateCart = (id,action) =>{
-    
-    if(action==="add"){
-      cart.push(id)
-      console.log(cart)
-    }
-
-    if(action==="del"){
-      cart = cart.filter((prod_id)=> prod_id!==id)
-      console.log(cart)
-    }    
-    
-    $('#cart_items_json').val(JSON.stringify(cart))
-    console.log(JSON.stringify(cart))
-
-
-  }
-
-
-
-
-
-
-
 
   // adding product cart
   $(".buy-btn").on("click", function () {
     var btn_id = $(this).attr("id");
     addToCart(btn_id);
+
+    //adding styles on buy-button click
     $(this).text("Added");
-
     $(this).parent().children(".remove-btn").css("display", "inline");
-
     // Add a class to change the button color
-    $(this).removeClass("btn-success");
-    $(this).addClass("btn-primary");
+    $(this).removeClass("btn-success-light");
+    $(this).addClass("btn-success");
     $(this).addClass("added");
     //updating the cart
-
-    updateCart(btn_id,"add")
-
-
-
+    cart.push(btn_id);
+    cart_items_json.value = JSON.stringify(cart);
   });
 
   $(".remove-btn").on("click", function () {
-
-    var btn_id = $(this).parent().children(".buy-btn").attr('id')
+    var btn_id = $(this).parent().children(".buy-btn").attr("id");
     $(this).parent().children(".buy-btn").text("Add to cart");
-    $(this).parent().children(".buy-btn").removeClass("btn-primary");
-    $(this).parent().children(".buy-btn").addClass("btn-success");
+    $(this).parent().children(".buy-btn").removeClass("btn-success");
+    $(this).parent().children(".buy-btn").addClass("btn-success-light");
 
     $(this).css("display", "none");
     console.log($(this).parent().children(".buy-btn").attr("id"));
     var prod_id = $(this).parent().children(".buy-btn").attr("id");
 
-    var cart = JSON.parse(localStorage.getItem("cart"));
-    cart[prod_id] = false;
-    localStorage.setItem("cart", JSON.stringify(cart));
+    var cart_local = JSON.parse(localStorage.getItem("cart"));
+    delete cart_local[prod_id];
+    localStorage.setItem("cart", JSON.stringify(cart_local));
 
     // updating the cart
+    cart.pop(btn_id);
+    cart_items_json.value = JSON.stringify(cart);
+  });
 
-    updateCart(btn_id,"del")
+  var order = [];
 
+  if (cart !== null) {
+    for (var i in cart) {
+      var obj = {
+        id: i,
+        qty: 1,
+      };
+      order.push(obj);
+    }
+  }
 
+  //change the state of the elements when cart page loads, based on the added items.
+
+  for (i in cart_storage) {
+    if (cart_storage[i] === 1) {
+      $("#" + i).click();
+      
+    }
+  }
+
+  // clear cart button
+
+  $("#clear-cart-btn").click(() => {
+    localStorage.setItem("cart", JSON.stringify({}));
+    cart = [];
+    cart_items_json.value = JSON.stringify(cart);
+    var cart_button = $("#cart-btn").click();
+  });
+
+  // remove button on the cart page
+  $(".rmv-btn").on("click", function () {
+    // Get the parent element and set its display property to none
+    $(this).parent().parent().css("display", "none");
+    var prod_id = $(this).attr("id");
+    let id = prod_id.charAt(prod_id.length - 1);
+
+    cart.pop("pr" + id);
+    cart_items_json.value = JSON.stringify(cart);
+
+    //updating local storage
+
+    let cart_loc = JSON.parse(localStorage.getItem("cart"));
+
+    delete cart_loc["pr" + id];
+
+    localStorage.setItem("cart", JSON.stringify(cart_loc));
+    if (Object.keys(cart_loc).length == 0) {
+      $("#cart-btn").click();
+    }
+    console.log(cart_loc);
   });
 
   console.log(cart)
+  console.log(cart_storage)
+  console.log(cart_items_json.value)
+
+
+  //order array that is initialized only when we go to the cart
+  let order_arr = []
+  
+
+  
+  
+
+
 });
